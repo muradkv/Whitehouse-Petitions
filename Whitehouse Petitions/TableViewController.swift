d5d34990ffa624e2bc879c4e485f001a6960cdc7
murad <muradkv@gmail.com>
2019-05-07 23:02:05 +0300

    Add TableView with Navigation and TabBar Controllers

---
+++ Whitehouse Petitions/TableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by murad on 07/05/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
