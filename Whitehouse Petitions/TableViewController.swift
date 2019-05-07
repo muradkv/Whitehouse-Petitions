78539cb7d15fb2be474c338cea6751749d8f9f39
murad <muradkv@gmail.com>
2019-05-07 23:16:36 +0300

    Add basic ui and show test text on tableView

--- Whitehouse Petitions/TableViewController.swift
+++ Whitehouse Petitions/TableViewController.swift
@@ -10,21 +10,33 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    var petitions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        petitions.append("Check")
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title text"
+        cell.detailTextLabel?.text = "Detail text"
+        
+        return cell
     }
 
 }
