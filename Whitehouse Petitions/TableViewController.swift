0bd3a3a36b08eab12d25348b45a66b5fdd16833a
murad <muradkv@gmail.com>
2019-05-08 23:40:43 +0300

    Add second TabBarItem that show popular petitions
    Create ViewController and add it to the tabBarController.viewControllers

--- Whitehouse Petitions/TableViewController.swift
+++ Whitehouse Petitions/TableViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class TableViewController: UITableViewController {
 
     var petitions = [Petition]()
+    var urlString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
